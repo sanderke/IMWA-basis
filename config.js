ba5565755854b7e1684a7b23fa01ea7ff39ba0e1
WijnandIHW <104829403+WijnandIHW@users.noreply.github.com>
2023-10-09 17:03:29 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  //subtitle: "IMWA Kunstwerken Gegevenscatalogus",
+   //publishDate: "2023-04-26", 
+  logos: [
+  {src: 'https://www.aquo.nl//logos/aquo_logo.svg',
+    href: "http://aquo.nl",
+    alt: "Aquo",
+    width: 252,
+    height: 130,
+    id: 'IMWA-KW'}]  ,
+  editors: [],
+  authors: [
+    {
+      name: "Informatiehuis Water",
+	  mailto: "servicedesk@ihw.nl",
+	  note: "033 203 50 97",
+      companyURL: "http://www.ihw.nl",
+    }],
+  shortName: "IMWA-KW",
+  pubDomain: "IMWA-Kunstwerken",
+  github: "https://github.com/Aquo-standaard/IMWA-KW",
+  previousPublishDate: "2023-04-26",
+  previousStatus: "AQ-WV",
+  localBiblio: {},
+};
