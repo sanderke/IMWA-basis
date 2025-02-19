173973be7d03c3ea02d9092c91abb8392009272a
WijnandIHW <104829403+WijnandIHW@users.noreply.github.com>
2025-02-19 16:26:56 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,83 @@
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  // title is verplicht! Neem hier de titel van het document op ----------------------
+  title: "Informatiemodel Water Basis",
+  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
+  specStatus: "wv",                   // Werkversie
+  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "def",              // Vastgestelde versie
+  //specStatus: "basis",            // Basis Document
+  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
+  //specType: "NO",                 // Norm
+  //specType: "ST",                 // Standaard
+ specType: "IM",                 // Informatie Model
+  //specType: "PR",                 // Praktijkrichtlijn
+  //specType: "HR",                     // HandReiking
+  //specType: "WA",                 // Werkafspraak
+  //specType: "BD",                 // Beheer Documentatie
+  //specType: "AL",                 // Algemeen document
+  //specType: "BP",                 // Best Practice
+  //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
+  //-- zie: https://geonovum.github.io/handleiding-tooling/ReSpec/#pubdomain
+  pubDomain: "https://aquo-standaard.github.io/IMWA-basis",
+  //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
+  //license: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //license: "cc0",                 // Public Domain Dedication
+  license: "cc-by",                 // Attribution, met bronvermelding
+  //-- TODO shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
+  shortName: "IMWA Basis",
+  
+  //edDraftURI = De URI van de draft version. Deze wordt automatisch afgeleid van de github URI; maar kan hier overschreven worden. 
+	//edDraftURI: ["https://geonovum.github.io", "/", "shortName"],
+
+  //-- publishDate is verplicht -------------------------------------------------------
+  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
+  publishDate: "2025-02-19",
+  //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
+  //publishVersion: "0.0.2",
+  //previousVersion: "0.0.1",
+  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
+  //previousPublishDate: "2014-05-01",
+  //previousMaturity: "CV",
+  //-- TODO: de namen van de Editor(s) / Redacteur(en)---------------------------------------
+  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
+  editors:
+    [],
+  //-- de namen van de auteur(s) ------------------------------------------------------
+  //-- vul in: per auteur: name:, company:, companyURL: -------------------------------
+  authors: [
+    {
+      name: "Informatiehuis Water",
+	  mailto: "servicedesk@ihw.nl",
+	  note: "033 203 50 97",
+      companyURL: "http://www.ihw.nl",
+    }],
+  // TODO: Vul de github URL in.
+  //neem hier de URL van de github repository op waar het respec document in staat
+  github: "https://github.com/Aquo-standaard/IMWA-basis",
+  // Create PDF and link to file in header (optional):
+  // TODO: Change the filename as preferred.
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "template.pdf",
+      },
+  ],
+  // Lokale lijst voor bibliografie
+  // - Kijk eerst naar de beschikbare www.specref.org .
+  // - Kijk daarna in de organisatieconfig.
+  // - Voeg dan pas hieronder toe.
+  localBiblio: 
+  {
+    MIM12: {
+      id: "MIM12",
+      title: "MIM - Metamodel Informatie Modellering (Versie 1.2)",
+      href: "https://docs.geostandaarden.nl/mim/def-st-mim-20240613/",
+      status: "Definitief",
+      publisher: "Geonovum",
+      date: "2024-06-13"
+    }
+  }
+};
