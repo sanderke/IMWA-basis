af7f7591647e9009a30b6a04d46034f843fa4776
WijnandIHW <104829403+WijnandIHW@users.noreply.github.com>
2025-03-06 17:52:27 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  specStatus: "WV",
+  specType: "IM",
+  //subtitle: "IMWA Basis ",
+   //publishDate: "2023-04-26", 
+  logos: [
+  {src: 'https://www.aquo.nl//logos/aquo_logo.svg',
+    href: "http://aquo.nl",
+    alt: "Aquo",
+    width: 252,
+    height: 130,
+    id: 'IMWA-Basis'}]  ,
+  editors: [],
+  authors: [
+    {
+      name: "Informatiehuis Water",
+	  mailto: "servicedesk@ihw.nl",
+	  note: "033 203 50 97",
+      companyURL: "http://www.ihw.nl",
+    }],
+  shortName: "IMWA-WBasis",
+  pubDomain: "IMWA-Basis",
+  github: "https://github.com/Aquo-standaard/IMWA-Basis",
+  previousPublishDate: "2023-04-26",
+  previousStatus: "WV",
+  localBiblio: {},
+};
